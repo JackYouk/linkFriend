c63544893b110bc43090df666cc85e5beab21e1e
Jack Youkstetter <jackyoukstetter1@gmail.com>
2022-06-01 23:06:52 -0700

    repo init, added basic html with js and script links, added pseudo code, commented the endpoints I will use for data fetching

---
+++ assets/js/script.js
@@ -0,0 +1,39 @@
+// endpoints
+
+// Phish checker
+// https://phishstats.info:2096/api/
+// Docs: https://phishstats.info/
+
+// Mozilla https security check
+// https://http-observatory.security.mozilla.org/api/v1
+// Docs: https://github.com/mozilla/http-observatory/blob/master/httpobs/docs/api.md
+
+// CheckPhish
+// https://developers.checkphish.ai/api
+// https://checkphish.ai/docs/checkphish-api/
+
+
+
+
+// data ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+// API #1
+// API #2
+// API #3
+// API #4
+// API #5
+
+// app ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+// cross check different apis, return value of trustworthiness
+
+// UI ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+// root
+const root = $('#root');
+
+// header
+// input bar
+// scale
+
+// more info page
