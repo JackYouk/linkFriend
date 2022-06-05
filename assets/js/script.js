ace8a5164156d4d3526460cd79e936577b5c3162
Jack Youkstetter <jackyoukstetter1@gmail.com>
2022-06-05 02:27:40 -0700

    created UI with jquery

--- assets/js/script.js
+++ assets/js/script.js
@@ -53,7 +53,55 @@ fetch(`https://http-observatory.security.mozilla.org/api/v1/analyze?host=${websi
 const root = $('#root');
 
 // header
+const headerContainer = $('<div class="row">');
+let headerH1 = $('<h1 class="col-12 bg-primary text-white d-flex justify-content-center">')
+    .text('LinkFriend');
+headerContainer.append(headerH1);
+root.append(headerContainer);
+
 // input bar
+const inputContainer = $('<div class="row">');
+let inputArea = $('<div class="col-12 d-flex justify-content-center">');
+    let inputGroup = $('<div class="input-group m-3 col-6">');
+        let inputInput = $('<input type="text" class="form-control" placeholder="Enter Link Here :)" aria-label="" aria-describedby="button-addon2">');
+        let inputBtn = $('<button class="btn btn-outline-secondary" type="button" id="button-addon2">')
+            .text('✓');
+        inputGroup.append(inputInput);
+        inputGroup.append(inputBtn);
+    inputArea.append(inputGroup);
+inputContainer.append(inputArea);
+root.append(inputContainer);
+
+// scanning label
+function genScanLabel(status){
+    const scanContainer = $('<div class="row">');
+    let scanArea = $('<div class="col-12 d-flex justify-content-center">');
+        let scanDiv = $('<div class"bg-primary">')
+            let scanGif = $('<iframe src="https://giphy.com/embed/JGUMPPTMRCVPbSncVF" width="100" height="100" frameBorder="0" class="giphy-embed" allowFullScreen>');
+            scanDiv.append(scanGif);
+            let scanP = $('<p class="">')
+                .text('Scanning...');
+            scanDiv.append(scanP);
+        scanArea.append(scanDiv);
+    scanContainer.append(scanArea);
+    root.append(scanContainer);
+}
+genScanLabel();
+
+
+
 // scale
+function genScale(grade){
+    const scaleContainer = $('<div class="row">');
+    let scaleArea = $('<div class="col-12 d-flex justify-content-center">');
+        let scaleBarGroup = $('<div class="progress col-6 m-5">');
+            let scaleBar = $('<div class="progress-bar" role="progressbar">');
+            scaleBarGroup.append(scaleBar);
+        scaleArea.append(scaleBarGroup);
+    scaleContainer.append(scaleArea);
+    root.append(scaleContainer);
+}
+genScale();
+
 
 // more info page
